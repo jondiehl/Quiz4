dc62a9660c9092522204b4ad5f68809f22fa83d8
jondiehl <diehl@myrtle.cs.moravian.edu>
2014-04-23 18:02:16 -0400

    Init Commit

---
+++ Quiz4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz4
+//
+//  Created by Jon Diehl on 2/7/14.
+//  Copyright (c) 2014 Jon Diehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Q4LeftController.h"
+#import "Q4RightController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz4/Q4RightController.h
@@ -0,0 +1,13 @@
+//
+//  Q4RightController.h
+//  Quiz4
+//
+//  Created by Jon Diehl on 2/7/14.
+//  Copyright (c) 2014 Jon Diehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Q4RightController : UIViewController
+
+@end
