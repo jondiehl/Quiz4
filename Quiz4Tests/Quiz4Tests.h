90861fa5ca88a837a99926e52abbd2f419bb6da3
jondiehl <diehl@myrtle.cs.moravian.edu>
2014-04-23 18:00:24 -0400

    Init Commit

---
+++ Quiz4Tests/Quiz4Tests.h
@@ -0,0 +1,13 @@
+//
+//  Quiz4Tests.h
+//  Quiz4Tests
+//
+//  Created by Jon Diehl on 2/7/14.
+//  Copyright (c) 2014 Jon Diehl. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Quiz4Tests : SenTestCase
+
+@end
---
+++ Quiz4Tests/Quiz4Tests.m
@@ -0,0 +1,32 @@
+//
+//  Quiz4Tests.m
+//  Quiz4Tests
+//
+//  Created by Jon Diehl on 2/7/14.
+//  Copyright (c) 2014 Jon Diehl. All rights reserved.
+//
+
+#import "Quiz4Tests.h"
+
+@implementation Quiz4Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Quiz4Tests");
+}
+
+@end
